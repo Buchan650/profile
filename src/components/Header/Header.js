ed67e99ff0d32c422b368e0b92b4b932b77a7c9b
Breon Curry <breoncurry@Breons-MacBook-Pro.local>
2023-07-05 23:15:04 +0900

    Fixed menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,10 +23,10 @@ function Header() {
                     <button className='dd-title' onClick={handleOpen}><i id='hamburger' class="fa-solid fa-bars"></i></button>
                     {open? (
                     <div className='menu'>
-                        <NavLink index className='menu-item'><button>About Me</button></NavLink>
-                        <NavLink to="/skills" className='menu-item'><button>Skills</button></NavLink>
-                        <NavLink to="/projects" className='menu-item'><button>Projects</button></NavLink>
-                        <NavLink to="/contact-info" className='menu-item'><button>Contact Info</button></NavLink>
+                        <NavLink index className='menu-item'><button onClick={handleOpen}>About Me</button></NavLink>
+                        <NavLink to="/skills" className='menu-item' ><button onClick={handleOpen}>Skills</button></NavLink>
+                        <NavLink to="/projects" className='menu-item'><button onClick={handleOpen}>Projects</button></NavLink>
+                        <NavLink to="/contact-info" className='menu-item'><button onClick={handleOpen}>Contact Info</button></NavLink>
                     </div>
                     ) : null}
                 </div>
